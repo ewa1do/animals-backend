a3a4e9d07728ebc8a7c3234267131be10aeb2558
ewa1do <this.eduardovera@gmail.com>
2022-11-07 20:26:12 -0400

    :bricks: Connecting database

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,31 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: `${__dirname}/../config.env` });
+
 import app from './app';
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-  console.log('Server running on PORT: ' + PORT);
-});
+const DB_NAME = process.env.DATABASE_NAME as string;
+const DB_USER = process.env.DATABASE_USER as string;
+const DB_PASSWORD = process.env.DATABASE_PASSWORD as string;
+const DB_URI = process.env.DATABASE_URI as string;
+
+const DB_CONFIG = DB_URI?.replace('<NAME>', DB_NAME)
+  .replace('<USER>', DB_USER)
+  .replace('<PASSWORD>', DB_PASSWORD);
+
+(async function () {
+  try {
+    await mongoose.connect(DB_CONFIG);
+    console.log('✅ Database Connection Enabled');
+
+    app.listen(PORT, () => {
+      console.log('✅ Server Connection Enabled');
+      console.log('🎧 Listening Requests on PORT: ' + PORT + '\n💚 Have fun');
+    });
+  } catch (err) {
+    console.log(err);
+  }
+})();
